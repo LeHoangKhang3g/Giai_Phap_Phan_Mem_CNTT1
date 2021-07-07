8d625e1ffdf4ce5913bfa8133146baf35476fdbb
TranLinh101h <0306191040@caothang.edu.vn>
2021-07-07 20:47:48 +0700

    update DAO

--- Session2/SESSION2/DAO/AssetDAO.cs
+++ Session2/SESSION2/DAO/AssetDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace DAO
 {
     public class AssetDAO
     {
+        private Session2Entities _ss2 = new Session2Entities();
+        public List<AssetDTO>()
+    {
+
+    }
     }
 }
