b0e5ec55daf972a464d5380d66a8ced42d6f0a96
LeHoangKhang3g <khangxyz3g@gmail.com>
2021-07-09 15:26:30 +0700

    Update Login GUI

---
+++ Session2/SESSION2/BUS/EmployeeBUS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using DTO;
+
+namespace BUS
+{
+    public class EmployeeBUS
+    {
+        private EmployeeDAO _employeeDAO = new EmployeeDAO();
+
+        public EmployeeDTO CheckAccount(string userName, string pass)
+        {
+            return _employeeDAO.CheckAccount(userName, pass);
+        }
+    }
+}
--- Session2/SESSION2/GUI/frmLogin.cs
+++ Session2/SESSION2/GUI/frmLogin.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DTO;
+using BUS;
 
 namespace GUI
 {
     public partial class frmLogin : Form
     {
+        private EmployeeBUS _employeeBUS = new EmployeeBUS();
+        public static EmployeeDTO employeeDTO_HienHanh = new EmployeeDTO();
         public frmLogin()
         {
             InitializeComponent();
@@ -29,7 +33,29 @@ namespace GUI
                 MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-
+            EmployeeDTO EM = _employeeBUS.CheckAccount(txtUsername.Text, txtPass.Text);
+            if (EM == null)
+            {
+                MessageBox.Show("Thông tin đăng nhập sai", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                employeeDTO_HienHanh = EM;
+                this.Hide();
+                if(EM.isAdmin==true)
+                {
+                    frmEM_Management_Admin fEM_Management_Admin = new frmEM_Management_Admin();
+                    fEM_Management_Admin.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    frmEM_Management fEm_Management = new frmEM_Management();
+                    fEm_Management.ShowDialog();
+                    this.Show();
+                }    
+            }
         }
     }
 }
